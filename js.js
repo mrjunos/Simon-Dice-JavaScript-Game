be6e435547299a9493c9ffd665d4992585e32e1d
JCano <jjcadu@gmail.com>
2018-11-14 17:51:53 -0500

    Mejorando el sistema de aumento de nivel

--- js.js
+++ js.js
@@ -3,6 +3,7 @@ const violeta = document.getElementById("violeta")
 const naranja = document.getElementById("naranja")
 const verde = document.getElementById("verde")
 const btnEmpezar = document.getElementById("btnEmpezar")
+const ultimoNivel = 10
 
 class Juego {
 	constructor(){
@@ -13,6 +14,7 @@ class Juego {
 
 	inicializar(){
 		this.elegirColor = this.elegirColor.bind(this)
+		this.siguienteNivel = this.siguienteNivel.bind(this)
 		btnEmpezar.classList.add("hide") //btnEmpezar.style = "display: none"
 		this.nivel = 1
 		this.colores = {
@@ -21,13 +23,18 @@ class Juego {
 			naranja,
 			verde
 		}
+		celeste.num = 0
+		violeta.num = 1
+		naranja.num = 2
+		verde.num = 3
 	}
 
 	generarSecuencia(){
-		this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+		this.secuencia = new Array(ultimoNivel).fill(0).map(n => Math.floor(Math.random() * 4))
 	}
 
 	siguienteNivel(){
+		this.subnivel = 0
 		this.iluminarSecuencia()
 		this.agregarEventosClick()
 	}
@@ -41,6 +48,19 @@ class Juego {
 			case 2:
 			return naranja
 			case 3:
+			return verde
+		}
+	}
+
+	colorDelColor(c){
+		switch(c){
+			case 'celeste':
+			return celeste
+			case 'violeta':
+			return violeta
+			case 'naranja':
+			return naranja
+			case 'verde':
 			return verde
 		}
 	}
@@ -68,8 +88,31 @@ class Juego {
 		this.colores.verde.addEventListener("click", this.elegirColor)
 	}
 
+	eliminarEventosClick(){
+		this.colores.celeste.removeEventListener("click", this.elegirColor)
+		this.colores.violeta.removeEventListener("click", this.elegirColor)
+		this.colores.naranja.removeEventListener("click", this.elegirColor)
+		this.colores.verde.removeEventListener("click", this.elegirColor)
+	}
+
 	elegirColor(ev){
-		console.log(ev)
+		const nombreColor = ev.target.dataset.color
+		const color = this.colorDelColor(ev.target.dataset.color)
+		this.iluminarColor(color)
+		if(color.num === this.secuencia[this.subnivel]){
+			this.subnivel++
+			if(this.subnivel === this.nivel){
+				this.nivel++
+				this.eliminarEventosClick()
+				if(this.nivel === (ultimoNivel+1)){
+					//Ganó
+				}else{
+					setTimeout(this.siguienteNivel(), 1500)
+				}
+			}
+		}else{
+			//Perdió 
+		}
 	}
 }
 
